8bb83cf40db5eb4639769eb7cb65150fc7ddb572
hkholghi <hkholghi@abingtonfriends.net>
2016-11-28 16:56:55 -0500

    Sample regular expression codes

---
+++ MatchingADecimalNumbersNum1.java
@@ -0,0 +1,18 @@
+import java.util.regex.*;
+/**
+ * Given an array of decimal numbers write a regular expression
+ * pattern that retrieves all the numbers except for the last
+ * one, 720p.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MatchingADecimalNumbersNum1
+{
+    public void run(){
+        System.out.print("\f");
+        String[] numbers = {"3.14529", "-255.34","128","1.9e10","23,340.00","720p"};
+        
+        // write the necessary statemnets
+    }
+}
