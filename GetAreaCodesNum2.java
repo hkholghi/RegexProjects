265ddb34c63787a3d06303c83486f15d378433c9
hkholghi <hkholghi@abingtonfriends.net>
2016-11-30 15:16:58 -0500

    added GetAreaCodesNum2.java

---
+++ GetAreaCodesNum2.java
@@ -0,0 +1,18 @@
+import java.util.regex.*;
+/**
+ * An array phoneNumbers has a list of phone numbers that are
+ * represented as strings.  Write a regular expression
+ * pattern that retrieves all the area codes.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class GetAreaCodesNum2
+{
+    public void run(){
+        System.out.println("\f");
+        String[] phoneNumbers ={"415-555-1234", "650-555-2345","(416)555-3456",
+                "202 555 4567", "4035555678","1 416 555 9292"};
+
+    }
+}
